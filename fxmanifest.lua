4719860ce24fb149a0dbd2a17904addfadc73913
PumaATS <spencer.a51tech@gmail.com>
2025-05-20 20:48:03 +0100

    Version Bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,14 +2,14 @@
     Script Name : PD Reception
     Author      : Riptide Studios
     Copyright   : © 2025 Riptide Studios
-    Version     : 1.0.2.1
+    Version     : 1.0.3.0
     Description : This configuration file is not intended to be edited.
                   Unauthorized modifications may cause unexpected behavior.
 ]]
 
-name "riptide_bell"
+name "riptide_reception"
 author "Riptide Studios"
-version "1.0.2.1"
+version "1.0.3.0"
 description "PD Reception Bell"
 fx_version "cerulean"
 game "gta5"
